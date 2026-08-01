362b3ef012ab53b97299351b1b83833ba4b56c2e
Yurii <olesich2007@gmail.com>
2026-08-01 20:49:55 +0300

    do my final 5th nodejs hw 05-mail-and-img

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -28,6 +29,7 @@ app.use(express.json());
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 
